5d3a207524e395877f7cfa7793e9926c9631cac2
Gizem Yiğit <gizemyigit38@outlook.com>
2023-10-12 21:53:50 +0300

    Add project files.

---
+++ eTicket/Data/MovieCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace eTicket.Data
+{
+    //bir çok film türünü barındıracağı için bir enum yapısı oluştururuz
+    public enum MovieCategory
+    {
+        Action = 1,//start index ekledik burdan yola çıkarak diğer değerlere id atanacaktır
+        Comedy,
+        Drama,
+        Documentary
+    }
+}
---
+++ eTicket/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using eTicket.Data;
+
+namespace eTicket.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double price { get; set; }
+
+        public string ImageURL { get; set; }
+        public DateTime StartdDate { get; set; }
+        public DateTime EndDate { get; set;}
+
+        public MovieCategory MovieCategory { get; set; }
+
+    
+    }
+}
